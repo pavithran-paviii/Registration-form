8b17adb60d21cb9e608280acf63d129672a76daa
pavithran <pavithranr65@gmail.com>
2022-06-29 02:48:44 +0530

    completed validation and UI

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputFields = ({ inputs, onchange }) => {
+  const [focus, setFocus] = useState(false);
+  const { ...inputsSpread } = inputs;
+
+  const handleFocus = () => {
+    setFocus(true);
+  };
   return (
     <div className="inputParent">
       <label htmlFor={inputs.id}>{inputs.label}</label>
       <input
-        type={inputs.type}
-        placeholder={inputs.placeholder}
-        id={inputs.id}
-        required
+        // type={inputs.type}
+        // placeholder={inputs.placeholder}
+        // id={inputs.id}
         onChange={onchange}
+        {...inputsSpread}
+        onBlur={handleFocus}
+        focused={focus.toString()}
       />
+      <span className="errorMessage">{inputs.error}</span>
     </div>
   );
 };
--- src/data/inputFieldsData.js
+++ src/data/inputFieldsData.js
@@ -3,13 +3,18 @@ export const inputFieldData = [
     label: "Name",
     type: "text",
     placeholder: "Enter your name",
+    error: "Username should only contain letters",
     id: "name",
+    required: true,
+    pattern: "^[a-zA-Z-]+$",
   },
   {
     label: "Email",
     type: "email",
     placeholder: "Enter your email",
+    error: "Enter valid email address",
     id: "email",
+    required: true,
   },
   {
     label: "Mobile",
@@ -34,33 +39,3 @@ export const inputFieldData = [
     id: "state",
   },
 ];
-
-//   <div className="inputParent">
-//     <label htmlFor="email">Email</label>
-//     <input
-//       type="email"
-//       placeholder="Enter your email"
-//       id="email"
-//       required
-//     />
-//   </div>
-//   <div className="inputParent">
-//     <label htmlFor="mobile">Mobile</label>
-//     <input
-//       type="tel"
-//       placeholder="Enter your mobile number"
-//       id="mobile"
-//     />
-//   </div>
-//   <div className="inputParent">
-//     <label htmlFor="country">Country</label>
-//     <CountryInput id="country" />
-//   </div>
-//   <div className="inputParent">
-//     <label htmlFor="city">City</label>
-//     <input type="text" placeholder="Enter your City" id="city" />
-//   </div>
-//   <div className="inputParent">
-//     <label htmlFor="state">State</label>
-//     <input type="text" placeholder="Enter your State" id="state" />
-//   </div>
